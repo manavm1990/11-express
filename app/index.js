92b5c169246bb96e7cdfa5a6a40c345b6cc8ad4b
Manav Misra <manavm@visionwebsoft.com>
2022-12-15 20:21:54 -0600

    Add example of dynamic param

--- app/index.js
+++ app/index.js
@@ -1,12 +1,28 @@
 import express from "express";
-import terms from "./db/terms.json" assert { type: "json" };
+import termsData from "./db/terms.json" assert { type: "json" };
 
 // Start express
 const app = express();
 const port = 3001;
 
-app.get("/api", (_, res) => {
-  res.json(terms);
+app.get("/api/terms", (_, res) => {
+  res.json(termsData);
+});
+
+// The ':' represents a DYNAMIC PARAMETER. (e.g. '/api/terms/WHATEVERIWANT')
+// We can see the name of this parameter as a key in the 'req.params.'
+app.get("/api/terms/:term", (req, res) => {
+  const { term } = req.params;
+
+  const requestedTerm = termsData.find(
+    (t) => t.term.toUpperCase() === term.toUpperCase()
+  );
+
+  if (requestedTerm) {
+    res.json(requestedTerm);
+  } else {
+    res.status(404).json({ error: `Term ${term} not found. :(` });
+  }
 });
 
 app.listen(port, () => {
