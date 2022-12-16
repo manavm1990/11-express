030ea969f9363bf5de19e0a08fbbddaca5e04eb5
Manav Misra <manavm@visionwebsoft.com>
2022-12-15 19:47:46 -0600

    Send back imported JSON

--- app/index.js
+++ app/index.js
@@ -1,21 +1,12 @@
 import express from "express";
+import terms from "./db/terms.json" assert { type: "json" };
 
 // Start express
 const app = express();
 const port = 3001;
 
-app.get("/", (_, res) => {
-  res.send(
-    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
-  );
-});
-
 app.get("/api", (_, res) => {
-  res.json({
-    term: "api",
-    description:
-      "An application programming interface, is a computing interface that defines interactions between multiple software intermediaries",
-  });
+  res.json(terms);
 });
 
 app.listen(port, () => {
